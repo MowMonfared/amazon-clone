a452906e7a407dd4098821d6b3ca1bd207061669
MowMonfared <monfared.mohammad@gmail.com>
2024-07-01 15:03:45 -0400

    Make the cart quantity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -128,6 +128,12 @@ buttons.forEach((button) => {
       });
     }
 
-    console.log(cart);
+    let cartQuantity = 0;
+
+    cart.forEach((item) => {
+      cartQuantity += item.quantity;
+    });
+
+    document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
   });
 });
