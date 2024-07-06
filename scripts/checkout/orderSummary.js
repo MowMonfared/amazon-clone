828d5f168d723c9e8240dc2fe1401a4d87d3504e
MowMonfared <monfared.mohammad@gmail.com>
2024-07-06 17:00:11 -0400

    Make payment summary interactive

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -7,9 +7,7 @@ import {
   getDeliveryOption,
 } from '../../data/deliveryOptions.js';
 
-const today = dayjs();
-const deliveryDate = today.add(7, 'days');
-console.log(deliveryDate.format('dddd, MMMM D'));
+import { renderPaymentSummary } from './paymentSummary.js';
 
 //Looping through cart array to generate HTML
 
@@ -119,8 +117,9 @@ export function renderOrderSummary() {
       const container = document.querySelector(
         `.js-cart-item-container-${productId}`
       );
-
       container.remove();
+
+      renderPaymentSummary();
     });
   });
 
@@ -129,6 +128,7 @@ export function renderOrderSummary() {
       const { productId, deliveryOptionId } = element.dataset;
       updateDeliveryOption(productId, deliveryOptionId);
       renderOrderSummary();
+      renderPaymentSummary();
     });
   });
 }
