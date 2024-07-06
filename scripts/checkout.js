2d257723d878d07991123f90fbab8fb695c6192e
MowMonfared <monfared.mohammad@gmail.com>
2024-07-06 12:18:05 -0400

    Make delivery options interactive

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,4 @@
-import { cart, removeFromCart } from '../data/cart.js';
+import { cart, removeFromCart, updateDeliveryOption } from '../data/cart.js';
 import { products } from '../data/products.js';
 import { formatCurrency } from './utils/money.js';
 import { hello } from 'https://unpkg.com/supersimpledev@1.0.1/hello.esm.js';
@@ -101,7 +101,9 @@ function deliveryOptionHTML(matchingProduct, cartItem) {
     const isCheceked = deliveryOption.id === cartItem.deliveryOptionId;
 
     html += `
-              <div class="delivery-option">
+              <div class="delivery-option js-delivery-option"
+              data-product-id="${matchingProduct.id}"
+              data-delivery-option-id="${deliveryOption.id}">
                     <input
                       type="radio"
                       ${isCheceked ? 'checked' : ''}
@@ -130,5 +132,12 @@ document.querySelectorAll('.js-delete-link').forEach((link) => {
     );
 
     container.remove();
+  });
+});
+
+document.querySelectorAll('.js-delivery-option').forEach((element) => {
+  element.addEventListener('click', () => {
+    const { productId, deliveryOptionId } = element.dataset;
+    updateDeliveryOption(productId, deliveryOptionId);
   });
 });
