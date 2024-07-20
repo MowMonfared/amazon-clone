9391e63ad88f5c55bf4d6ec4a66e639a32811f58
MowMonfared <monfared.mohammad@gmail.com>
2024-07-20 14:44:03 -0400

    Learn error handling

--- data/products.js
+++ data/products.js
@@ -75,6 +75,9 @@ export function loadProductsFetch() {
         return new Product(productDetails);
       });
       console.log('load products');
+    })
+    .catch((error) => {
+      console.log('Unexpected error. Please try again later.');
     });
   return promise;
 }
@@ -99,6 +102,10 @@ export function loadProducts(func) {
     console.log('load products');
 
     func();
+  });
+
+  xhr.addEventListener('error', (error) => {
+    console.log('Unexpected error. Please try again later.');
   });
 
   xhr.open('GET', 'https://supersimplebackend.dev/products');
